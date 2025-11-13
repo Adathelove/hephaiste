4fabf0f112d148ed1bf1787b255c4724409a9b94
Ada Love <113861258+Adathelove@users.noreply.github.com>
2025-11-13 11:22:13 -0700

    initial commit

---
+++ basic.test.js
@@ -0,0 +1,6 @@
+import { test, expect } from "vitest";
+
+test("dummy test", () => {
+  expect(1 + 1).toBe(2);
+});
+
---
+++ index.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+console.log("Hephaiste sparks the forge.");
+
