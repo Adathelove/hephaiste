076e387866c1193a3e6600001b80bddc0a1ecafb
Ada Love <113861258+Adathelove@users.noreply.github.com>
2025-11-13 14:59:44 -0700

    ✨ feat(ui): Out put hellow with command line

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 #!/usr/bin/env node
 
-console.log("Hephaiste sparks the forge.");
+const args = process.argv.slice(2);
+
+if (args[0] === "greet") {
+    const nameIndex = args.indexOf("--name");
+    const name = nameIndex !== -1 ? args[nameIndex + 1] : "friend";
+    console.log(`Forge greets you, ${name}.`);
+    process.exit(0);
+}
+
+console.log("Hephaiste: no command given. Try: hephaiste greet --name Ada");
 
